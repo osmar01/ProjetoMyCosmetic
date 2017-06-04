00d15bd2c86744d2e63cc17e3444d700663b30aa
osmar01 <osmarj.frota@gmail.com>
2017-06-04 01:56:16 -0400

    Carrinho de Compras

--- web/js/CarrinhoDeCompras.js
+++ web/js/CarrinhoDeCompras.js
@@ -2,20 +2,34 @@ function adicionarProduto() {
     
     if (!localStorage.getItem("carrinho")) {
         produto = (document.getElementById('nome').value);
+        produtoPreco = (document.getElementById('preco').value);
+        produtoQuantidade = (document.getElementById('quantidade').value);
+        total = produtoQuantidade * produtoPreco;
         //criando carrinho (array) de produtos      
         carrinho = [];
         //incluindo produto no carrinho (array)
-        carrinho[0] = produto;
+        carrinho[3] = produto+"------->";
+        carrinho[2] = produtoPreco+"------>";        
+        carrinho[1] = produtoQuantidade+"------->";
+        carrinho[0] = total+"<br>";
+        
         localStorage.setItem("carrinho", JSON.stringify(carrinho));
     } else {
         //-- atualizando carrinho  --//
         //adicionando novo produto ao carrinho
         produto = (document.getElementById('nome').value);
+        produtoPreco = (document.getElementById('preco').value);
+        produtoQuantidade = (document.getElementById('quantidade').value);
+        total = produtoQuantidade * produtoPreco;
+        
         //recuperando carrinho sessão
         carrinho = JSON.parse(localStorage.getItem("carrinho"));
         //recuperando quantidade produtos no carrinho
         qtde = carrinho.length;
-        carrinho[qtde] = produto;
+        carrinho[qtde+3] = produto+"------->";
+        carrinho[qtde+2] = produtoPreco+"------->";
+        carrinho[qtde+1] = produtoQuantidade+"------->";
+        carrinho[qtde] = total+"<br>";
         //atualizando carrinho da sessão
         localStorage.setItem("carrinho", JSON.stringify(carrinho));
     }
@@ -29,8 +43,8 @@ function listarProdutos() {
         //exibindo informações do carrinho
         carrinho = JSON.parse(localStorage.getItem("carrinho"));
         for (i = 0; i < carrinho.length; i++) {
-            itens = carrinho[i] + "<br>" + itens;
-        }
+            itens = carrinho[i] + "" + itens;
+        }       
         document.getElementById("itensCarrinho").innerHTML = itens;
     }
 }
